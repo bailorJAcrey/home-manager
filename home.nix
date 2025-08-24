51c7ac1af51da418f333ac1e82baf3627d7c8ef8
sudonym_ <bailorjacrey@gmail.com>
2025-08-23 23:59:24 -0500

    Add todoist cli tool

--- home.nix
+++ home.nix
@@ -57,6 +57,7 @@
     gcc
     firefox
     bat
+    todoist
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
